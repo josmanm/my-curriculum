424c45e3b19d2e2b0ddf79c6df8e8dc3b63fff35
Josman Santiago Muñoz Mera <57530390+josmanm@users.noreply.github.com>
2025-10-10 18:10:11 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/my-curriculum/', // importante para GH Pages
+  base: "/my-curriculum/",
   plugins: [react()],
-})
+});
